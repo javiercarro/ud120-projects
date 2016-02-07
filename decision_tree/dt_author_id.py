5469ccb77198673cf428a98c469416c62a3583dd
Javier Carro <@>
2016-02-07 19:56:36 +0000

    Lesson 3

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -22,10 +22,53 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
+
 #########################################################
 ### your code goes here ###
 
+def writeToFile(filename, text, mode):
+  print text
+  with open(filename, mode) as text_file:
+    text_file.write(text+"\n")
+
+
+from sklearn import tree
+
+def accDecTree():
+  clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+
+  start_time = time()
+  clf.fit(features_train, labels_train)
+  elapsed = time()-start_time
+  text = "Training time (DecTree): {0}s".format(round(elapsed, 3))
+  writeToFile("DecTree_output.txt", text, "w")
+
+  acc = clf.score(features_test, labels_test)
+  text = "Accuracy (DecTree): {0}s".format(round(acc, 3))
+  writeToFile("DecTree_output.txt", text, "a")
+
+def featuresOriginal():
+  n_features = len(features_train[0])
+  text = "Features (Original): {0}".format(round(n_features, 3))
+  writeToFile("DecTree_output.txt", text, "a")
+
+def accDecTree_1PercentFeatures():
+  clf = tree.DecisionTreeClassifier(min_samples_split = 40)
+
+  start_time = time()
+  clf.fit(features_train, labels_train)
+  elapsed = time()-start_time
+  text = "Training time (DecTree 1%features): {0}s".format(round(elapsed, 3))
+  writeToFile("DecTree_output.txt", text, "a")
+
+  acc = clf.score(features_test, labels_test)
+  text = "Accuracy (DecTree 1%features): {0}s".format(round(acc, 3))
+  writeToFile("DecTree_output.txt", text, "a")
 
 #########################################################
 
+#accDecTree()
+#featuresOriginal()
+accDecTree_1PercentFeatures()
 
+#########################################################
