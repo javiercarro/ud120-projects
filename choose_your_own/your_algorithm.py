753f5ed2bfbbe66914a387b8b848aef44335d65b
Javier Carro <@>
2016-02-14 22:05:11 +0000

    Lesson 4

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -30,15 +30,100 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+import sys
+from time import time
+
+def writeToFile(filename, text, mode):
+  print text
+  with open(filename, mode) as text_file:
+    text_file.write(text+"\n")
 
 
 
+# Algorithms to try
+# 1. k nearest neighors - classic, simple, easy to understand
+# 2. Ensemble methods): meta classifiers built from (usually) decision trees
+#   2.1. Adaboost (boosting)
+#   2.2. Random Forest (averaging)
 
+# 1. k nearest neighors - classic, simple, easy to understand
+def KNearestNeigh(k):
+  from sklearn.neighbors import KNeighborsClassifier
+  
+  clf = KNeighborsClassifier(n_neighbors = k)
 
+  start_time = time()
+  clf.fit(features_train, labels_train)
+  elapsed = time()-start_time
+  text = "Training time (kNearestNeigh:{0}): {1}s".format(k, round(elapsed, 3))
+  writeToFile("ChooseYourOwn_output.txt", text, "a")
 
+  acc = clf.score(features_test, labels_test)
+  text = "Accuracy (kNearestNeigh:{0}): {1}".format(k, round(acc, 3))
+  writeToFile("ChooseYourOwn_output.txt", text, "a")
 
-
-try:
+  try:
     prettyPicture(clf, features_test, labels_test)
-except NameError:
+  except NameError:
     pass
+
+#   2.1. Adaboost (boosting)
+def AdaBoost(k):
+  from sklearn.ensemble import AdaBoostClassifier
+
+  clf = AdaBoostClassifier(n_estimators = k)
+
+  start_time = time()
+  clf.fit(features_train, labels_train)
+  elapsed = time()-start_time
+  text = "Training time (AdaBoost:{0}): {1}s".format(k, round(elapsed, 3))
+  writeToFile("ChooseYourOwn_output.txt", text, "a")
+
+  acc = clf.score(features_test, labels_test)
+  text = "Accuracy (AdaBoost:{0}): {1}".format(k, round(acc, 3))
+  writeToFile("ChooseYourOwn_output.txt", text, "a")
+
+  #try:
+    #prettyPicture(clf, features_test, labels_test)
+  #except NameError:
+    #pass
+
+#   2.2. Random Forest (averaging)
+def RandomForest(k):
+  from sklearn.ensemble import RandomForestClassifier
+
+  clf = RandomForestClassifier(n_estimators = k)
+
+  start_time = time()
+  clf.fit(features_train, labels_train)
+  elapsed = time()-start_time
+  text = "Training time (RandomForest:{0}): {1}s".format(k, round(elapsed, 3))
+  writeToFile("ChooseYourOwn_output.txt", text, "a")
+
+  acc = clf.score(features_test, labels_test)
+  text = "Accuracy (RandomForest:{0}): {1}".format(k, round(acc, 3))
+  writeToFile("ChooseYourOwn_output.txt", text, "a")
+
+  #try:
+    #prettyPicture(clf, features_test, labels_test)
+  #except NameError:
+    #pass
+
+text = "File initialization"
+writeToFile("ChooseYourOwn_output.txt", text, "w")
+
+#KNearestNeigh(3)
+#KNearestNeigh(5)
+#KNearestNeigh(10)
+#KNearestNeigh(50)
+
+#AdaBoost(10)
+#AdaBoost(50)
+#AdaBoost(100)
+#AdaBoost(200)
+
+RandomForest(10)
+#RandomForest(50)
+#RandomForest(100)
+#RandomForest(200)
+
