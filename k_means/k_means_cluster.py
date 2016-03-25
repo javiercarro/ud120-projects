cdf3cd50f821fd963e826620eec149d82ef24431
Javier Carro <javier.carrocalabor@gmail.com>
2016-03-25 17:19:37 +0200

    KMeans - Enf of Lesson

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -15,9 +15,14 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
 
+def writeToFile(filename, text, mode):
+  print text
+  with open(filename, mode) as text_file:
+    text_file.write(text+"\n")
 
 
-def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
+
+def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2", f3_name="feature 3"):
     """ some plotting code designed to help you visualize your clusters """
 
     ### plot each cluster with a different color--add more colors for
@@ -42,14 +47,22 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
+sal = []
+for k in data_dict.keys():
+  if data_dict[k]["salary"] != 'NaN': sal.append(data_dict[k]["salary"])
+text = "Max Salary: {0}".format(max(sal))
+writeToFile("kMeans_output.txt", text, "w")
+text = "Min Salary: {0}".format(min(sal))
+writeToFile("kMeans_output.txt", text, "a")
 
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,19 +71,20 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+for f1, f2, f3 in finance_features:
+    plt.scatter( f1, f2, f3 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
-
+from sklearn.cluster import KMeans
+pred = KMeans(n_clusters=2).fit_predict(finance_features)
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_3features.pdf", f1_name=feature_1, f2_name=feature_2, f3_name=feature_3)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
